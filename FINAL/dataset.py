bf9940a38eb920b87e9b2ba6c586c6e6e7be8e9c
penguin1109 <penguin1109@gmail.com>
2021-07-24 02:10:14 +0900

    change dataset.py

--- FINAL/dataset.py
+++ FINAL/dataset.py
@@ -5,7 +5,7 @@ import PIL.Image as pil_image
 import torchvision.transforms as transforms
 
 
-class CroppedDataset(Dataset):
+class CroppedDataset(torch.utils.data.Dataset):
     # read and change image size(3, 2448, 3264) -> (3, 612, 816)
     def __init__(self, x_img_dirs, y_img_dirs, split, is_train, crop_size = (612, 816)):
         # img_dirs는 이미지의 저장 경로가 담겨있는 리스트의 형태
@@ -14,6 +14,13 @@ class CroppedDataset(Dataset):
         self.target = y_img_dirs
         self.split = int(len(self.input) * split)
         self.w, self.h = crop_size[0], crop_size[1]
+
+        if is_train:
+          self.input = self.input[:self.split]
+          self.target = self.target[:self.split]
+        else:
+          self.input = self.input[self.split:]
+          self.target = self.target[self.split:]
 
         self.samples = []
         # target, label이미지의 저장 경로를 하나씩 리스트에 넣어줌
@@ -26,32 +33,35 @@ class CroppedDataset(Dataset):
     def __getitem__(self, idx):
         inputs, labels = self.samples[idx]
         in_img, label_img = pil_image.open(inputs).convert('RGB'), pil_image.open(labels).convert('RGB')
-
+        imgtonp = lambda x: np.array(x).astype(np.float32)
         
-        resized_x, resized_y = transforms.Resize((self.w, self.h))(in_img), transforms.Resize((self.w, self.h))
-        resized_x, resized_y = transforms.ToTensor()(resized_x), transforms.ToTensor()(resized_y)
+        transform = []
+        transform.append(transforms.Resize((self.w, self.h)))
+        transform.append(transforms.ToTensor())
+        transform = transforms.Compose(transform)
 
-        w, h = in_img.shape[0], in_img.shape[1]
+        resized_x = transform(in_img)
+        resized_y = transform(label_img)
 
-        scale = w/self.w
+        h, w = in_img.size[0], in_img.size[1]
+
+        scale = int(w/self.w)
         img_nums = scale ** 2
 
-        w_start, h_start = 0, 0
+        start_x, start_y = 0, 0
         data = []
         data.append({'input' : resized_x, 'target' : resized_y})
         for i in range(1, scale+1):
             for j in range(1, scale+1):
-                w_end, h_end = w_start + i*self.w, h_start + i *self.h
-                crop_x = in_img.crop((w_start, h_start, w_end, h_end))
-                crop_y = label_img.crop((w_start, h_start, w_end, h_end))
+                end_x, end_y = start_x + self.h, start_y + self.w
+                crop_x = in_img.crop((start_x, start_y, end_x, end_y))
+                crop_y = label_img.crop((start_x, start_y, end_x, end_y))
 
                 crop_x = np.array(crop_x).astype(np.float32)
                 crop_y = np.array(crop_y).astype(np.float32)
 
-                # pil_image.read로 읽었기 떄문에 (w, h, 3)이므로
-                # 입력으로 모델에 넣어 줄 떄에는 (3, w, h)로 바꾸어야 함
-                crop_x = np.transpose(crop_x, axes = [2, 0, 1])
-                crop_y = np.transpose(crop_y, axes = [2, 0, 1])
+                # pil_image.read로 읽었기 떄문에 (h, w, 3)이므로
+                # 입력으로 모델에 넣어 줄 떄에는 (3, h, w)로 바꾸어야 함
 
                 # scale 0-255.0 -> 0-1
                 crop_x = crop_x/255.0
@@ -61,6 +71,8 @@ class CroppedDataset(Dataset):
                 crop_y = transforms.ToTensor()(crop_y)
 
                 data.append({'input' : crop_x, 'target' : crop_y})
+                start_x += self.h
+            start_y += self.w
+            start_x = 0
 
         return data           
-
